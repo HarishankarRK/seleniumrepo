e6a7d224c45f4cfc523590c9eafb65b02a3c32c8
HarishankarRK <crkharikaushik@gmail.com>
2024-09-21 00:23:50 +0530

    Add files via upload

---
+++ week1/Chrome.java
@@ -0,0 +1,24 @@
+package selenium1;
+
+public class Chrome 
+{
+	public static void main(String[] args) {
+		
+		
+		float Versiontype = 91.0f;
+		String developer ="Google";
+		boolean isbeta =false;
+		int releaseYear =2008;
+		char shortcutkey = 'C';
+		System.out.println("Release year  :"   + releaseYear);
+		System.out.println("float   :" + Versiontype);
+		System.out.println("String   :" + developer);
+		System.out.println("Boolean value   :" + isbeta);
+		System.out.println("Char   :" + shortcutkey);
+		
+		
+		
+		}
+	
+	
+}
